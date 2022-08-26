e01db6e7dbc3fc2e5cb84ec72afc77b0f0fe9837
Aditya Honkalas <aditya.honkalas@ibm.com>
2022-08-26 12:21:16 -0700

    OCP CLI-plugins deployment and testing automation
    Signed-off-by: Aditya Honkalas <aditya.honkalas@ibm.com>

---
+++ playbooks/roles/ocp-cli-plugins/files/oc-test-plugin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# optional argument handling
+if [[ "$1" == "version" ]]
+then
+    echo "1.0.0"
+    exit 0
+fi
+
+# optional argument handling
+if [[ "$1" == "config" ]]
+then
+    echo $KUBECONFIG
+    exit 0
+fi
+
+echo "This is the oc-custom-plugin !"
